1b992c9256bc1c82408ad4bcc4b77ef6a1d6715a
Nayanna Nara <nayanna501@gmail.com>
2020-09-16 15:48:44 -0300

    teste

--- siteecommerce/settings.py
+++ siteecommerce/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'siteecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db_projetoecommerce.sqlite3')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
